a7d3b5dc95b36a3d4bac3317fc0fa78c15dd19e7
Callum Shaw <callum0shaw@gmail.com>
2023-02-14 12:01:26 +0200

    feat: redirect index to home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        about: 'storyblok/About'
+      },
+    })
+  ],
 });
